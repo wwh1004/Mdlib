e7d60a10b1df177d775b645076234272344098b6
文煌 <wwh1004@hotmail.com>
2019-08-26 16:07:21 +0800

    rename

--- Mdlib/Properties/AssemblyInfo.cs
+++ Mdlib/Properties/AssemblyInfo.cs
@@ -8,6 +8,6 @@ using System.Runtime.CompilerServices;
 #if DEBUG
 [assembly: InternalsVisibleTo("Mdlib.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001007bc906c343345c436a792d4f09d888cc3d6eb94e68d88811e173f7a486ec341b4bf04014cb9ed8ea568c6aa28e8230f5c95b692c9f57455b106942fe156a4d72b5999946a1defabec06d31c3c10a431c2d038de1ac8fd13fa4577be8499eef641a1242b38815c8c2e8d43252090147168686874bd6e60c582a07e58630510199")]
 #endif
-[assembly: AssemblyVersion("0.0.3.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.3.1")]
+[assembly: AssemblyFileVersion("0.0.3.1")]
 [assembly: AssemblyInformationalVersion("v.B.e.t.a")]
